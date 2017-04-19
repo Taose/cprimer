ca197c0332720f3b6d99a910c7c396a2d17519c6
taose <lucifer.stmichael@gmail.com>
2017-04-19 10:51:53 +0100

    Exercise Section 1.3 completed

--- main.cpp
+++ main.cpp
@@ -1,24 +1,21 @@
 /*
- * Exercise 1.2
- *
- * Exercise 1.3
- * 	Select appropriate file naming convention, compile and run.
+ * Exercise Section 1.3
  *
  */
 
-#include <iostream> //for std::cout and std::endl
+#include <iostream>
 
 int main()
 {
-	std::cout << "Hello World :3" << std::endl; //Exercise 1.3
-	std::cout << 2 * 2 << std::endl; //Exercise 1.4
-	//Exercise 1.5 is being skipped because it's superfluous.
+	//Exercise 1.7
+	//	// /* Incorrectly
 
-	int v1{1}, v2{2};
-	/*	//The following is not legal as it doesn't output the second line anywhere.
-	   std::cout << "The sum of " << v1;
-				<< " and " << v2;
-				<< " is " << v1 + v2 << std::endl;
-	 */
+	//	Nested Comments */
+
+	//Exercise 1.8
+	std::cout << "/*"; //Legal
+	std::cout << "*/"; //Legal
+	//std::cout << /* "*/" */; //Not legal
+	std::cout << /* "*/" /* "/*" */; //Legal but...
 	return 0;
 }
