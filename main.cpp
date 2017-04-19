27f4870495ea8f9a46d9eb20e13d3ff776d8fe21
taose <lucifer.stmichael@gmail.com>
2017-04-19 07:56:06 +0100

    Exercise 1.2 completed

--- main.cpp
+++ main.cpp
@@ -1,12 +1,24 @@
 /*
- * Exercise 1.1.1
+ * Exercise 1.2
  *
- * Exercise 1.1
+ * Exercise 1.3
  * 	Select appropriate file naming convention, compile and run.
  *
  */
 
+#include <iostream> //for std::cout and std::endl
+
 int main()
 {
-	return -1; //Exercise 1.2
+	std::cout << "Hello World :3" << std::endl; //Exercise 1.3
+	std::cout << 2 * 2 << std::endl; //Exercise 1.4
+	//Exercise 1.5 is being skipped because it's superfluous.
+
+	int v1{1}, v2{2};
+	/*	//The following is not legal as it doesn't output the second line anywhere.
+	   std::cout << "The sum of " << v1;
+				<< " and " << v2;
+				<< " is " << v1 + v2 << std::endl;
+	 */
+	return 0;
 }
