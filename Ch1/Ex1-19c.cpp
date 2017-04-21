ce969d8631cbf48bef625730f425a83507b23716
taose <lucifer.stmichael@gmail.com>
2017-04-21 09:08:42 +0100

    Fixing ex1.19

--- Ch1/Ex1-19c.cpp
+++ Ch1/Ex1-19c.cpp
@@ -5,18 +5,20 @@ Print each number in the range specified by those two integers.*/
 int main()
 {
 	int integer1{0}, integer2{0};
-	std::cout << "Please enter two integers: " << std::endl;
-	std::cin >> integer1 >> integer2;
+	std::cout << "Please enter two integers (ctrl+d to exit): " << std::endl;
 
-	if (integer1 <= integer2)
-		std::cout << integer1++ << std::endl
+	while (std::cin >> integer1 >> integer2)
+	{
+		std::cout << std::endl; //New line :D
+		if (integer1 <= integer2)
+			while (integer1 <= integer2)
+				std::cout << integer1++ << std::endl;
+		else //First integer is *bigger*
+			std::cerr << "Error: The first value must be smaller than the second value" << std::endl;
 
+		std::cout << "Please enter two more integers: " << std::endl;
+	}
 
-	while (integer1 <= integer2)
-		std::cout << integer1++ << std::endl; //We want it to increment *after* it has printed
-
-	while (integer1 >= integer2)
-		std::cout << integer1-- << std::endl;
 
 	return 0;
 }
